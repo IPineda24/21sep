8e9429addd9e7171ba7e1aa4a969de605e028582
LAPT-CSH-72 <cshtd.computacion@gmail.com>
2024-09-23 09:20:01 -0600

    :sparkles: Add a mesage

--- main.js
+++ main.js
@@ -2,3 +2,8 @@ function mostrarImagen() {
     var img = document.getElementById("miImagen");
     img.style.display = "block"; // Muestra la imagen al hacer clic
 }
+
+function mostrarMensaje() {
+    var text = document.getElementById("elMensaje");
+    text.style.display = "block"; // Muestra la imagen al hacer clic
+}
