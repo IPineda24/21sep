afedbd11f91ad9544c3551e98337f10ea0889f19
LAPT-CSH-44 <cshtd.computacion@gmail.com>
2024-09-23 08:59:40 -0600

    Add Function Suma :sparkles:

--- main.js
+++ main.js
@@ -2,3 +2,9 @@ function mostrarImagen() {
     var img = document.getElementById("miImagen");
     img.style.display = "block"; // Muestra la imagen al hacer clic
 }
+
+function suma (a, b){
+    return a+b
+};
+
+console.log(suma);
